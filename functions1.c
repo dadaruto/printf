f3b3a87a53c5403adad0d55e446306e3d04261bf
DouglasKutwa <kutwadouglas90@gmail.com>
2023-04-25 23:36:04 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -17,15 +17,16 @@ int print_unsigned(va_list types, char buffer[],
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
+
 	num = convert_size_unsgnd(num, size);
 
-  if (num == 0)
+	if (num == 0)
 
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-  while (num > 0)
+	while (num > 0)
 	{
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
@@ -56,13 +57,13 @@ int print_octal(va_list types, char buffer[],
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
-  UNUSED(width);
+	UNUSED(width);
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
 		buffer[i--] = '0';
 	buffer[BUFF_SIZE - 1] = '\0';
 	while (num > 0)
-    
+
 	{
 		buffer[i--] = (num % 8) + '0';
 		num /= 8;
@@ -90,7 +91,7 @@ int print_octal(va_list types, char buffer[],
  */
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
-  
+
 {
 	return (print_hexa(types, "0123456789abcdef", buffer,
 		flags, 'x', width, precision, size));
